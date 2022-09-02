55452050341b275ff6c39265e9229c7cb99045bc
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-09-02 15:23:49 +0900

    [Fix]

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,6 @@
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
-      :clipped="clipped"
       fixed
       app
     >
@@ -28,7 +27,7 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
